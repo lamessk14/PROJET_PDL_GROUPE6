159812229c69f0f9203a855e11f61b36bc5abb28
maheleon <hmaykol@gmail.com>
2017-09-28 22:56:52 +0200

    Add test with API Jongo

---
+++ src/main/java/com/mako/testAPI/App.java
@@ -0,0 +1,13 @@
+package com.mako.testAPI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/mako/testAPI/Products.java
@@ -0,0 +1,53 @@
+package com.mako.testAPI;
+
+/**
+ * 
+ * PDL MIAGE 1718
+ * Projet #2 (OpenFoodFacts2CSV)
+ * Group 6
+ * 
+ * @authors HERNANDEZ Maykol, ADDA Raoul, MACKONGO Louise-Agnès, ZOHOUN Nellya, TCHIDIME Hugues
+ * @version 1.0
+ * @since 2017-09-21
+ * 
+ * Class to test the connection with OFF dump with the API Jongo
+ * 
+ */
+
+public class Products {
+
+	private long _id;
+	private String product_name_en;
+
+	public Products() {
+
+	}
+
+	public Products(int _id, String product_name_en) {
+		super();
+		this.product_name_en = product_name_en;
+		this._id = _id;
+	}
+
+	public String getName() {
+		return product_name_en;
+	}
+
+	public void setName(String product_name_en) {
+		this.product_name_en = product_name_en;
+	}
+
+	public long getId() {
+		return _id;
+	}
+
+	public void setId(int _id) {
+		this._id = _id;
+	}
+
+	@Override
+    public String toString() {
+        return (_id + " " + product_name_en);
+    }
+	
+}
