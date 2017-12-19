16db82301a8f12e7142d7d06f192cce4d4b92c98
lamessk14 <louise-agnes.essebe@etudiant.univ-rennes1.fr>
2017-12-19 13:45:45 +0100

    Add of documentation

--- MatrixSynthesizerWikipedia/src/main/java/fr/istic/pdl/groupe6/msw/ToCsv.java
+++ MatrixSynthesizerWikipedia/src/main/java/fr/istic/pdl/groupe6/msw/ToCsv.java
@@ -8,7 +8,26 @@ import org.supercsv.io.CsvMapWriter;
 import org.supercsv.io.ICsvMapWriter;
 import org.supercsv.prefs.CsvPreference;
 
+/**
+ * 
+ * PDL MIAGE 1718, Project #3 (MatrixSynthesizerWikipedia), Group 6
+ * 
+ * @author HERNANDEZ Maykol, ADDA Raoul, MACKONGO Louise-Agnès, ZOHOUN Nellya,
+ *         TCHIDIME Hugues, NGOUALEM Alvine
+ * @version 1.0
+ * @since 2017-10-31
+ * 
+ *        Class for creating csv file
+ * 
+ */
+
 public class ToCsv {
+	/**
+	 * 
+	 * @param map
+	 * @param header
+	 * @throws Exception
+	 */
 
 	public void writeWithCsvMapWriter(Map<String, String> map, String[] header) throws Exception {
 
