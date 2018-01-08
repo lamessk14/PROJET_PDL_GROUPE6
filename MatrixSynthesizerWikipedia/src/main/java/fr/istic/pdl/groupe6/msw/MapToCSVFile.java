7a5cd3de16e4dfd0b2f3e77aa29484c96bb47552
maheleon <hmaykol@gmail.com>
2018-01-08 21:05:40 +0100

    Fix export CSV

--- MatrixSynthesizerWikipedia/src/main/java/fr/istic/pdl/groupe6/msw/MapToCSVFile.java
+++ MatrixSynthesizerWikipedia/src/main/java/fr/istic/pdl/groupe6/msw/MapToCSVFile.java
@@ -20,7 +20,7 @@ import java.util.Set;
  * @version 1.0
  * @since 2017-10-31
  * 
- *        Class for creating a CSV file from an Map object
+ *        Class for creating a CSV file from a Map object
  * 
  */
 
@@ -33,7 +33,7 @@ public class MapToCSVFile {
 	 * 			Map<Integer, Map>
 	 * @throws IOException 
 	 */
-	public void mapToCSV(Map<Integer, Map> map) throws IOException {
+	public void mapToCSVComplex(Map<Integer, Map> map) throws IOException {
 		
 		String eol = System.getProperty("line.separator");
 		
@@ -44,63 +44,70 @@ public class MapToCSVFile {
 		File matrice = new File("files/matrice" + fileName + ".csv");
 		FileWriter outfile = new FileWriter(matrice);
 		
-		boolean b = true;
+		Compare comp = new Compare();
+		Set<String> keys = comp.getIntersection(map);
 		
-			for(Map.Entry<Integer, Map> entry : map.entrySet()) {
-				
-				if(b) {
-					Collection<String> keys = entry.getValue().keySet();
-					int i1 = 0;
-					Iterator<String> it1 = keys.iterator();
-					while(it1.hasNext()) {
-						String par = (String) it1.next();
-						
-						if(i1 != keys.size() - 1) {
-							try {
-								outfile.write(par + ",");
-							} catch (IOException e) {
-								System.out.println(e);
-								e.printStackTrace();
-							}
-						} else {
-							try {
-								outfile.write(par + eol);
-							} catch (IOException e) {
-								System.out.println(e);
-								e.printStackTrace();
-							}
-						}
-						i1++;
-					}
-					b = false;
+		Iterator<String> it2 = keys.iterator();
+		int i2 = 0;
+		while(it2.hasNext()) {
+			String par = (String) it2.next();
+			
+			if(i2 != keys.size() - 1) {
+				try {
+					outfile.write(par + ",");
+				} catch (IOException e) {
+					System.out.println(e);
+					e.printStackTrace();
 				}
-	
-				Collection<String> parametres = entry.getValue().values();
-				Iterator<String> it = parametres.iterator();
-				int i = 0;
-				while(it.hasNext()) {
-					String par = (String) it.next();
-					
-					if(i != parametres.size() - 1) {
-						try {
-							outfile.write(par + ",");
-						} catch (IOException e) {
-							System.out.println(e);
-							e.printStackTrace();
-						}
-					} else {
-						try {
-							outfile.write(par + eol);
-						} catch (IOException e) {
-							System.out.println(e);
-							e.printStackTrace();
-						}
-					}
-					i++;
+			} else {
+				try {
+					outfile.write(par + eol);
+				} catch (IOException e) {
+					System.out.println(e);
+					e.printStackTrace();
 				}
 			}
-			outfile.flush();
-			outfile.close();
+			i2++;
+		}
+		
+		
+		for(Map.Entry<Integer, Map> entry : map.entrySet()) {
+			
+			Map<String, String> element = entry.getValue();
+			Iterator<String> it3 = keys.iterator();
+			int i = 0;
+			while(it3.hasNext()) {
+				String keyRef = it3.next();
+				if(element.containsKey(keyRef)) {
+					try {
+						outfile.write(element.get(keyRef).replace(",", ""));
+					} catch (IOException e) {
+						System.out.println(e);
+						e.printStackTrace();
+					}
+				}
+				
+				
+				if(i != keys.size() - 1) {
+					try {
+						outfile.write(",");
+					} catch (IOException e) {
+						System.out.println(e);
+						e.printStackTrace();
+					}
+				}
+				i++;
+			}
+			try {
+				outfile.write(eol);
+			} catch (IOException e) {
+				System.out.println(e);
+				e.printStackTrace();
+			}
+			
+		}
+		outfile.flush();
+		outfile.close();
 	}
 	
 	/**
@@ -110,7 +117,7 @@ public class MapToCSVFile {
 	 * 			Map<Integer, Map>
 	 * @throws IOException 
 	 */
-	public void mapToCSVComplex(Map<Integer, Map> map) throws IOException {
+	public void mapToCSV(Map<Integer, Map> map) throws IOException {
 		
 		String eol = System.getProperty("line.separator");
 		
@@ -164,7 +171,7 @@ public class MapToCSVFile {
 				String keyRef = it3.next();
 				if(element.containsKey(keyRef)) {
 					try {
-						outfile.write(element.get(keyRef));
+						outfile.write(element.get(keyRef).replace(",", ""));
 					} catch (IOException e) {
 						System.out.println(e);
 						e.printStackTrace();
@@ -194,5 +201,4 @@ public class MapToCSVFile {
 		outfile.close();
 	}
 	
-
 }
