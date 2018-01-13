4e100b1920daf1a52704e1215a858beb022324d3
arsympa <arsympa@hotmail.fr>
2018-01-13 16:35:53 +0100

    Update ReadConfigFile.java

--- MatrixSynthesizerWikipedia/src/main/java/fr/istic/pdl/groupe6/msw/ReadConfigFile.java
+++ MatrixSynthesizerWikipedia/src/main/java/fr/istic/pdl/groupe6/msw/ReadConfigFile.java
@@ -1,50 +1,67 @@
 package fr.istic.pdl.groupe6.msw;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import org.json.simple.parser.JSONParser;
 
 public class ReadConfigFile {
+
 	List<String> listeToCompareWP = new ArrayList<String>();
 	List<String> listeToCompareWD = new ArrayList<String>();
-	
+	JSONParser parser = new JSONParser();
+
+	@SuppressWarnings("unchecked")
 	public List<String> getListWP() {
 
 		try {
-			File entree = new File("configFile.json");
-			BufferedReader br = new BufferedReader(new FileReader(entree));
-			String ligne;
 
-			while ((ligne = br.readLine()) != null) {
-				listeToCompareWP.add(ligne);
+			Object obj = parser.parse(new FileReader("configFile.json"));
+
+			JSONObject jsonObject = (JSONObject) obj;
+			JSONArray listNom = (JSONArray) jsonObject.get("ListNom");
+			JSONArray listId = (JSONArray) jsonObject.get("ListId");
+
+			Iterator<String> iteratorNom = listNom.iterator();
+			Iterator<Long> iteratorId = listId.iterator();
+
+			while (iteratorNom.hasNext()) {
+				listeToCompareWP.add(iteratorNom.next());
 			}
 
-			br.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.toString());
+			while (iteratorId.hasNext()) {
+				String valId = iteratorId.next().toString();
+				listeToCompareWP.add(valId);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return listeToCompareWP;
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	public List<String> getListWD() {
 
 		try {
-			File entree = new File("configFileWD.json");
-			BufferedReader br = new BufferedReader(new FileReader(entree));
-			String ligne;
 
-			while ((ligne = br.readLine()) != null) {
-				listeToCompareWD.add(ligne);
+			Object obj = parser.parse(new FileReader("configFileWD.json"));
+
+			JSONObject jsonObject = (JSONObject) obj;
+			JSONArray listNom = (JSONArray) jsonObject.get("ListNom");
+
+			Iterator<String> iteratorNom = listNom.iterator();
+
+			while (iteratorNom.hasNext()) {
+				listeToCompareWD.add(iteratorNom.next());
 			}
 
-			br.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return listeToCompareWD;
 	}
